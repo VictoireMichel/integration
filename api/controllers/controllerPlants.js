3724fd36bf9c1c9bd5aba14e53b9db08208d4251
VictoireMichel <56120737+VictoireMichel@users.noreply.github.com>
2020-10-12 12:21:00 +0200

    route pour 1 plante

--- api/controllers/controllerPlants.js
+++ api/controllers/controllerPlants.js
@@ -6,3 +6,13 @@ exports.getAll = function(req, res) {
         .catch(error => res.status(400).json({error}));
 };
 
+exports.getOne = function(req, res) {
+    Plants.findAll({
+        where: {
+            id: req.query.id
+        }
+    })
+        .then((results) => res.json(results))
+        .catch(error => res.status(400).json({error}));
+};
+
--- api/routes/routePlants.js
+++ api/routes/routePlants.js
@@ -4,6 +4,7 @@ const ctrPlants = require("../controllers/controllerPlants");
 
 // Get All route
 router.get("/all", (req, res)  => ctrPlants.getAll(req, res));
-//a
+//Get One route
+router.get("/one", (req, res)  => ctrPlants.getOne(req, res));
 
 module.exports = router;
