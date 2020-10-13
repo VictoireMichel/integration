2fae50973417bdc53640b7466d2c38bd9d68ac80
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2020-10-14 00:48:22 +0200

    update

--- api/app.js
+++ api/app.js
@@ -5,10 +5,4 @@ const seq = require("./config/config");
 const modelPlants = require("./models/modelPlants");
 const routePlants = require("./routes/routePlants");
 app.use("/plants", routePlants);
-
-app.get('/', (req,res) => {
-    res.send('Hello World changed');
-})
-
 app.listen(3000);
-
