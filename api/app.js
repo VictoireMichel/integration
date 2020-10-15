d2df9aa1f969dac40d2164ab5f577f36e680ca83
RodrigueMugisha <56130774+Rody96@users.noreply.github.com>
2020-10-15 09:35:00 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -9,7 +9,7 @@ app.use("/plants", routePlants);
 /* simple test pour avoir un rendu sur le '/' lors du déploiement sur Heroku */
 const PORT = process.env.PORT || '3000';
 app.get('/', (req,res) => {
-    res.send('Hello there is a change');
+    res.send('Hello there is a change again');
 });
 app.use("/filter", routeFilter);
 
