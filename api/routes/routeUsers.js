2fcbed997e3c5727f34d741cfae26f971b57606a
Igor Vandervelden <34266875+Gri097@users.noreply.github.com>
2020-10-22 10:33:05 +0200

    test fusion

--- api/routes/routeUsers.js
+++ api/routes/routeUsers.js
@@ -8,3 +8,25 @@ router.get("/all", (req, res)  => ctrUsers.getAll(req, res));
 router.get("/one", (req, res)  => ctrUsers.getOne(req, res));
 
 module.exports = router;
+
+module.exports = app => {
+    const Users = require("../controllers/customer.controller.js");
+
+    // Create a new Customer
+    app.post("/users", Users.create);
+
+    // Retrieve all Customers
+    app.get("/users", Users.findAll);
+
+    // Retrieve a single Customer with customerId
+    app.get("/users/:customerId", Users.findOne);
+
+    // Update a Customer with customerId
+    app.put("/users/:customerId", Users.update);
+
+    // Delete a Customer with customerId
+    app.delete("/users/:customerId", Users.delete);
+
+    // Create a new Customer
+    app.delete("/users", Users.deleteAll);
+};
