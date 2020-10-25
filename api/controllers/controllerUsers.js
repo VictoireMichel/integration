967900915219b69490d447a7174499d7cc330d5a
Louis Brochet <louisbrochet@outlook.com>
2020-10-25 10:00:53 +0100

    a

--- api/controllers/controllerUsers.js
+++ api/controllers/controllerUsers.js
@@ -2,8 +2,8 @@ const Users = require("../models/modelUsers");
 
 exports.getAll = function(req, res) {
     Users.findAll()
-        .then(results => res.json(results))
-        .catch(error => res.status(400).json({error}));
+.then(results => res.json(results))
+    .catch(error => res.status(400).json({error}));
 };
 
 exports.getOne = function(req, res) {
